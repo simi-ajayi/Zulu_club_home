184353fda44779c9122f7bc3c0035e5874b02760
dishant345 <dishant@junkiescoder.com>
2024-08-28 19:31:15 +0530

    mobile responsive UI

--- js/app.js
+++ js/app.js
@@ -1,87 +1,5 @@
 // Get ready to become a Fashion Brand overnight
 
-// const Retailers = document.querySelector(".brand-subtext span:nth-child(1)");
-// const Wholesalers = document.querySelector(".brand-subtext span:nth-child(2)");
-// const Manufacturers = document.querySelector(
-//   ".brand-subtext span:nth-child(3)"
-// );
-// const Brands = document.querySelector(".brand-subtext span:nth-child(4)");
-// const Partners = document.querySelector(".brand-subtext span:nth-child(5)");
-// const DefaultFAQ = document.querySelector("#Default-FAQ");
-// const FAQ1 = document.querySelector("#FAQ1");
-// const FAQ2 = document.querySelector("#FAQ2");
-// const FAQ3 = document.querySelector("#FAQ3");
-// const FAQ4 = document.querySelector("#FAQ4");
-// const FAQ5 = document.querySelector("#FAQ5");
-
-// Retailers.addEventListener("click", () => {
-//   Retailers.classList.add("active");
-//   Wholesalers.classList.remove("active");
-//   Manufacturers.classList.remove("active");
-//   Brands.classList.remove("active");
-//   Partners.classList.remove("active");
-//   FAQ1.classList.remove("d-none");
-//   FAQ2.classList.add("d-none");
-//   FAQ3.classList.add("d-none");
-//   FAQ4.classList.add("d-none");
-//   FAQ5.classList.add("d-none");
-//   DefaultFAQ.classList.add("d-none");
-// });
-
-// Wholesalers.addEventListener("click", () => {
-//   Wholesalers.classList.add("active");
-//   Retailers.classList.remove("active");
-//   Manufacturers.classList.remove("active");
-//   Brands.classList.remove("active");
-//   Partners.classList.remove("active");
-//   FAQ1.classList.add("d-none");
-//   FAQ2.classList.remove("d-none");
-//   FAQ3.classList.add("d-none");
-//   FAQ4.classList.add("d-none");
-//   FAQ5.classList.add("d-none");
-//   DefaultFAQ.classList.add("d-none");
-// });
-
-// Manufacturers.addEventListener("click", () => {
-//   Wholesalers.classList.remove("active");
-//   Retailers.classList.remove("active");
-//   Manufacturers.classList.add("active");
-//   Brands.classList.remove("active");
-//   Partners.classList.remove("active");
-//   FAQ1.classList.add("d-none");
-//   FAQ2.classList.add("d-none");
-//   FAQ3.classList.remove("d-none");
-//   FAQ4.classList.add("d-none");
-//   FAQ5.classList.add("d-none");
-//   DefaultFAQ.classList.add("d-none");
-// });
-// Brands.addEventListener("click", () => {
-//   Wholesalers.classList.remove("active");
-//   Retailers.classList.remove("active");
-//   Manufacturers.classList.remove("active");
-//   Brands.classList.add("active");
-//   Partners.classList.remove("active");
-//   FAQ1.classList.add("d-none");
-//   FAQ2.classList.add("d-none");
-//   FAQ3.classList.add("d-none");
-//   FAQ4.classList.remove("d-none");
-//   FAQ5.classList.add("d-none");
-//   DefaultFAQ.classList.add("d-none");
-// });
-// Partners.addEventListener("click", () => {
-//   Wholesalers.classList.remove("active");
-//   Retailers.classList.remove("active");
-//   Manufacturers.classList.remove("active");
-//   Brands.classList.remove("active");
-//   Partners.classList.add("active");
-//   FAQ1.classList.add("d-none");
-//   FAQ2.classList.add("d-none");
-//   FAQ3.classList.add("d-none");
-//   FAQ4.classList.add("d-none");
-//   FAQ5.classList.remove("d-none");
-//   DefaultFAQ.classList.add("d-none");
-// });
-
 const elements = {
   Retailers: document.querySelector(".brand-subtext span:nth-child(1)"),
   Wholesalers: document.querySelector(".brand-subtext span:nth-child(2)"),
@@ -100,7 +18,9 @@ const faqs = {
 };
 
 const handleClick = (activeKey) => {
+  console.log(activeKey);
   Object.keys(elements).forEach((key) => {
+    console.log("5656",key)
     elements[key].classList.toggle("active", key === activeKey);
     faqs[key].classList.toggle("d-none", key !== activeKey);
   });
